613c28a9d1daecf978768d8e63669c322088d7cc
Craig Shewry <craigshewry@gmail.com>
2021-10-12 15:55:16 +0100

    added func to read pdgid and convert to string and latex string

--- LHEImport/defs.py
+++ LHEImport/defs.py
@@ -1,4 +1,7 @@
 import math
+import pandas as pd
+import importlib.resources as pkg_resources
+
 def convert_px_py_pz(px,py,pz):
     # transverse momentum
     pt = math.sqrt(math.fsum([math.pow(px, 2), math.pow(py, 2)]))
@@ -11,3 +14,9 @@ def convert_px_py_pz(px,py,pz):
         eta = math.copysign(float('inf'), pz)
         phi = 0
     return pt, eta, phi
+
+def pdgid_to_string(pdgid):
+    stream = pkg_resources.open_text(__package__, 'pdgid_string.csv')
+    pdgid_data = pd.read_csv(stream)
+    pdgid_data=pdgid_data.set_index('ID')
+    return pdgid_data.loc[pdgid]['Name'], pdgid_data.loc[pdgid]['Latex']
