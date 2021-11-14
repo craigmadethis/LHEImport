2283d77b8d02918c017e62ecb988e5ecebd9394c
Craig Shewry <craigshewry@gmail.com>
2021-11-14 22:47:00 +0000

    redefined particle class, redefined readlhe and the hdf5 converter

--- LHEImport/LHEImport2.py
+++ LHEImport/LHEImport2.py
@@ -1,5 +1,8 @@
 import xml.etree.ElementTree as ET
 import math
+import vector
+import pandas as pd
+import importlib.resources as pkg_resources
 
 class LHEEvent:
     def __init__(self, eventinfo, particles, weights=None, attributes=None):
@@ -17,29 +20,25 @@ class LHEEventInfo:
     def fromstring(cls, string):
         return cls(**dict(zip(cls.fieldnames, map(float,string.split()))))
 
+class LHEParticle(object):
+    def __init__(self, **kwargs):
+        self.px = 0.0
+        self.py = 0.0
+        self.pz = 0.0
+        self.e = 0.0
+        self.mass = 0.0
+        self.id= 0
+        self.__dict__.update(**kwargs)
+        self.pdgid=int(self.id)
+        self.fourvec = vector.obj(px=self.px,
+                                  py=self.py,
+                                  pz=self.pz,
+                                  E=self.e)
+        self.pt=self.fourvec.pt
+        self.phi=self.fourvec.phi
+        self.eta=self.fourvec.eta
+        # self.pdgid_string, self.pdgid_latex =pdgid_to_string(self.pdgid)
 
-def read_lhe(filepath):
-    for event, element in ET.iterparse(filepath, events=["end"]):
-        if element.tag == "event":
-            eventdict={}
-            ## here we're not extracting the info block
-            data = element.text.split("\n")[1:-1]
-            eventdata, particles = data[0], data[1:]
-            eventdict["eventinfo"] = LHEEventInfo.fromstring(eventdata)
-            eventdict["particles"] = []
-            # eventdict["weights"] = {}
-            # eventdict["attributes"] = element.attrib
-            for p in particles:
-                if not p.strip().startswith("#"):
-                    eventdict["particles"] += [Particle.fromstring(p)]
-            yield LHEEvent(eventinfo = eventdict["eventinfo"],
-                           particles=eventdict["particles"],
-                           # weights = eventdict["weights"],
-                           # attributes=eventdict["attributes"]
-                           )
-
-
-class Particle(object):
     fieldnames = [ "id",
         "status",
         "mother1",
@@ -54,47 +53,51 @@ class Particle(object):
         "lifetime",
         "spin",
     ]
-    def __init__(self, pdgid=0, status=0,
-                 # initial_state=False,
-                 # final_state=False,
-                 **kwargs):
-        self.pdgid=int(pdgid)
-        # build this function (guessing it takes pdgids and checks dict vals to give particle name)
-        # self.name=pdgid_to_string(self.pdgid)
-        # self.name=self.pdgid
-        self.status=int(status)
-        # self.final_state = final_state
-        # self.initial_state = initial_state
-        for k in ['pt', 'eta', 'phi', 'px', 'py', 'pz', 'energy', 'mass']:
-        # for k in ['px', 'py', 'pz', 'energy', 'mass']:
-            # __dict__ a method of adding params to a class
-            self.__dict__[k] = 0.0
-        self.__dict__.update(**kwargs)
-        if all([k in kwargs for k in ['px', 'py','pz']]):
-            pt, eta, phi = self.convert_px_py_pz(float(self.px), float(self.py), float(self.pz))
-            self.__dict__['pt'] = pt
-            self.__dict__['eta'] = eta
-            self.__dict__['phi'] = phi
-    ## when you print the object this is what is returned
-    # def __str__(self):
-    #     return "Particle {0}, PDGID{1}".format(self.barcode, self.pdgid)
 
     @classmethod
     def fromstring(cls,string):
         return cls(**dict(zip(cls.fieldnames, map(float, string.split()))))
 
-    @classmethod
-    def convert_px_py_pz(cls,px,py,pz):
-        # transverse momentum
-        pt = math.sqrt(math.fsum([math.pow(px, 2), math.pow(py, 2)]))
-        # total momentum
-        ptot = math.sqrt(math.fsum([math.pow(pt, 2), math.pow(pz, 2)]))
-        if pt != 0:
-            eta = math.asinh(pz / pt)
-            phi = math.asin(py / pt)
-        else:
-            eta = math.copysign(float('inf'), pz)
-            phi = 0
-        dphi = 
-        return pt, eta, phi
+    def __str__(self):
+        return "Particle, PDGID{0}".format( self.pdgid)
 
+
+def read_lhe(filepath):
+    for event, element in ET.iterparse(filepath, events=["end"]):
+        if element.tag == "event":
+            eventdict={}
+            ## here we're not extracting the info block
+            data = element.text.split("\n")[1:-1]
+            eventdata, particles = data[0], data[1:]
+            # extracting event info 
+            eventdict["eventinfo"] = LHEEventInfo.fromstring(eventdata)
+            # extracting weights and attributes
+            # eventdict["weights"] = {}
+            # eventdict["attributes"] = element.attrib
+            eventdict["particles"] = []
+            for p in particles:
+                if not p.strip().startswith("#"):
+                    eventdict["particles"] += [LHEParticle.fromstring(p)]
+            yield LHEEvent(eventinfo = eventdict["eventinfo"],
+                           particles=eventdict["particles"],
+                           # weights = eventdict["weights"],
+                           # attributes=eventdict["attributes"]
+                           )
+
+def tohdf5(data, filename, key, limit_events=False):
+    events = [d for d in data]
+    eventinfo= [e.eventinfo for e in events]
+    particles = [e.particles for e in events]
+    if limit_events:
+        df = pd.DataFrame({'event_info':eventinfo[:int(len(events)*0.1)],
+                           'particles':particles[:int(len(events)*0.1)]})
+        df.to_hdf(f"{filename}.h5", key=f"{key}")
+    else:
+        df = pd.DataFrame({'event_info':eventinfo, 'particles':particles})
+        df.to_hdf(f"{filename}.h5", key=f"{key}")
+
+# def pdgid_to_string(pdgid):
+#     stream = pkg_resources.open_text(__package__, 'pdgid_string.csv')
+#     pdgid_data = pd.read_csv(stream)
+#     pdgid_data=pdgid_data.set_index('ID')
+#     return pdgid_data.loc[pdgid]['Name'], pdgid_data.loc[pdgid]['Latex']
